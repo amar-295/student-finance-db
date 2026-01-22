bf3e802189a8415a5194a15dacb1e631fbf28d61
amar-295 <612amar@gmail.com>
2026-01-22 16:52:24 +0530

    Use separate type import for SubmitHandler

--- frontend/src/components/transactions/TransactionForm.tsx
+++ frontend/src/components/transactions/TransactionForm.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
-import { useForm, type SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import type { SubmitHandler } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 
